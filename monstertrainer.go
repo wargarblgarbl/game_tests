222044d5162068402b0d1d83adf803c0fcb4d042
Dobroslav Slavenskoj <dslavneskoj@fuze.com>
2017-03-10 23:51:35 -0500

    2

--- monstertrainer.go
+++ monstertrainer.go
@@ -1,60 +1,75 @@
+
+
 package main
 import (
 	"fmt"
-	"math/rand"
-	"time"
+//	"math/rand"
+//	"time"
 )
 
+var entities []*entity
 
 type entity struct  {
 	Name string
 	Title string
+
 	HP int
 	MP int
-
-	Strength int
-	Speed int
-	Intelligence int
+	STR int
+	INT int
 
 	HPcounter int
 	MPcounter int
-	StrengthCounter int
-	IntCounter int
-	SpeedCounter int
-	LazyCounter int
+	STRCount int
+	IntCount int
+	SPDCount int
+	LZYCount int
+
+	Speed int
+	Gender string
+	Type string
+
+	PosX int
+	PosY int
 }
 
-
 //So with the *entity in the func and &entity in the return this will be a pointer to the struct
-//Without the star, this will be a copy. Potentially useful for creating heros. 
-func createHero () entity  {
-	return entity {
-		Name : "Cheesecake",
-		Title : "TheBrave",
+//Without the star, this will be a copy. Potentially useful for creating heros.
+func createHero (name string) *entity  {
+	return &entity {
+		Name : name,
 		HP : 10,
 		MP : 10,
-		Strength : 3,
+		STR : 3,
 		Speed : 3,
-		Intelligence : 2,
-		HPcounter : 0,
-		MPcounter : 0,
-		StrengthCounter : 0,
-		SpeedCounter : 0,
-		LazyCounter : 0,
+		INT : 2,
 	}
 }
 
-
-//FOR EACH TYPE CREATE ENTITY. 
+//FOR EACH TYPE CREATE ENTITY.
 func populous() {
-
+	critters := []string{"human", "goblin", "cobold" }
+	for _, i := range critters {
+		hero := createHero(i)
+		fmt.Println(hero)
+		entities = append(entities, hero)
+	}
 }
 
-var hero = createHero()
+//var hero = createHero()
 
 
 
 func main () {
+	//init population
+	fmt.Println("initializing population"	)
+	populous()
+	for _, i := range entities {
+		fmt.Println(i.Name)
+	}
+	
+}
+/*
 	// main game loop
 		counter := 0
 
@@ -64,27 +79,27 @@ func main () {
 
 		switch {
 		case action == 0:
-			hero.LazyCounter++
+			hero.LZYCount++
 		case action == 1:
 			hero.HPcounter++
 		case action == 2:
 			hero.MPcounter++
 		case action == 3:
-			hero.StrengthCounter++
+			hero.STRCount++
 		case action == 4:
-			hero.SpeedCounter++
+			hero.SPDCount++
 		case action == 5:
-			hero.StrengthCounter++
+			hero.STRCount++
 		case action == 6:
 			hero.MPcounter++
 		case action == 7:
 			hero.HPcounter++
 		case action == 8:
-			hero.StrengthCounter++
+			hero.STRCount++
 		case action == 9:
 			hero.HPcounter++
 		case action == 10:
-			hero.LazyCounter++
+			hero.LZYCount++
 		}
 		switch {
 		case hero.HPcounter == 10 + (hero.HP/10):
@@ -92,26 +107,27 @@ func main () {
 			hero.HPcounter = 0
 		case hero.MPcounter == 10 + (hero.MP/10):
 			hero.MP++
-			hero.MPcounter = 0 
-		case hero.StrengthCounter == 10 + (hero.Strength/10):
-			hero.Strength++
-			hero.StrengthCounter = 0 
-		case hero.SpeedCounter == 10 - (hero.LazyCounter/10):
+			hero.MPcounter = 0
+		case hero.STRCount == 10 + (hero.STR/10):
+			hero.STR++
+			hero.STRCount = 0
+		case hero.SPDCount == 10 - (hero.LZYCount/10):
 			hero.Speed++
-			hero.SpeedCounter = 0
-		case hero.LazyCounter == 20:
+			hero.SPDCount = 0
+		case hero.LZYCount == 20:
 			hero.Speed--
-			hero.LazyCounter = 0 
+			hero.LZYCount = 0
 		}
 		fmt.Println("HP: ", hero.HP)
 		fmt.Println("MP: ", hero.MP)
-		fmt.Println("Strength", hero.Strength)
+		fmt.Println("STR", hero.STR)
 		fmt.Println("Speed", hero.Speed)
-		fmt.Println(hero.LazyCounter)
+		fmt.Println(hero.LZYCount)
 		fmt.Println(counter)
 		fmt.Println("--------")
 				counter++
 
-		
+
 	}
 }
+*/
